0f6d77c00ba2952cc5c9bb7798d548938c8a217d
Sanjar17 <sanjarsaidov173@gmail.com>
2023-05-08 17:19:36 +0500

    camera added

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,75 @@
+import 'dart:io';
+
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+late List<CameraDescription> _cameras;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  _cameras = await availableCameras();
+  runApp(const CameraApp());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+/// CameraApp is the Main Application.
+class CameraApp extends StatefulWidget {
+  /// Default Constructor
+  const CameraApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  State<CameraApp> createState() => _CameraAppState();
 }
 
-class _MyAppState extends State<MyApp> {
+class _CameraAppState extends State<CameraApp> {
+  late CameraController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = CameraController(_cameras[0], ResolutionPreset.max);
+    controller.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+      setState(() {});
+    }).catchError((Object e) {
+      if (e is CameraException) {
+        switch (e.code) {
+          case 'CameraAccessDenied':
+            // Handle access errors here.
+            break;
+          default:
+            // Handle other errors here.
+            break;
+        }
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    if (!controller.value.isInitialized) {
+      return Container();
+    }
+    return MaterialApp(
+        home: Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          XFile f = await controller.takePicture();
+          controller.pausePreview();
+          print(f.path);
+          print(File(f.path));
+        },
+        child: Icon(Icons.camera),
+      ),
+      body: CameraPreview(controller),
+    ));
   }
 }
