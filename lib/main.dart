a080cbe1c29848aae9f3338cc8dbf1103ebb40cb
Sanjar17 <sanjarsaidov173@gmail.com>
2023-05-10 09:09:58 +0500

    auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
-import 'package:cross_file/cross_file.dart';
 
 void main() {
   runApp(const Placeholder());
